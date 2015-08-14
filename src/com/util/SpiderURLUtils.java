6ae2a8bbeae3b592038d4749efb05f034fb2117e
wenlinzou <daoxiji@gmail.com>
2015-08-14 16:04:55 +0800

    os：win10 再次提交增加了登陆的背景图

--- src/com/util/SpiderURLUtils.java
+++ src/com/util/SpiderURLUtils.java
@@ -12,27 +12,30 @@ import java.util.regex.Pattern;
 public class SpiderURLUtils {
 	 //发送代数据的HTTP 请求 -->网络爬虫
 	
-	public List<String> getDataByURL(String urlPath) throws IOException, Exception {
+	public List<String> getDataByURL(String urlPath) throws IOException {
 //		URL url = new URL("http://192.168.1.100:8080/myweb/mail.html");
 		List<String> htmlInfo = new ArrayList<String>();
 		
-		URL url = new URL(urlPath);
-		BufferedReader bufr = new BufferedReader(new InputStreamReader(url.openStream(),"UTF-8"));
-		String line = null;
-
+		URL url = null;
 //		String mail_reg = "\\w+@\\w+(\\.\\w+)+";
 //		String mail_reg = "\\S";
 //		Pattern p = Pattern.compile(mail_reg);
 		
-		while ((line = bufr.readLine()) != null) {
-			htmlInfo.add(line);
-//System.out.println(line);
-		/*
-			htmlInfo.add(m.group());
-			Matcher m = p.matcher(line);
-			while (m.find()) {
-				sb.append(m.group());
-			}*/
+		try {
+			url = new URL(urlPath);
+			BufferedReader bufr = new BufferedReader(new InputStreamReader(
+					url.openStream(), "UTF-8"));
+			String line = null;
+			while ((line = bufr.readLine()) != null) {
+				htmlInfo.add(line);
+				/*htmlInfo.add(m.group());
+					Matcher m = p.matcher(line);
+					while (m.find()) {
+						sb.append(m.group());
+					}*/
+			}
+		} catch (IOException e) {
+			System.out.println("找不到 " + urlPath);
 		}
 		return htmlInfo;
 		//document.close();
@@ -40,7 +43,6 @@ public class SpiderURLUtils {
 	public static void main(String[] args) throws Exception {
 		SpiderURLUtils s = new SpiderURLUtils();
 		List<String> temm = s.getDataByURL("http://www.open-open.com/lib/view/open1402448471822.html");
-
 
 	}
 }
